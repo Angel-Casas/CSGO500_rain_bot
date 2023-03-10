4e11a9e1e7220442a5e3a918c465297e3217b014
Ángel Casas Pescador <a.casas.lp@gmail.com>
2023-03-10 13:04:12 +0000

    axios and cheerio

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
-import puppeteer from 'puppeteer';
-import TelegramBot from 'node-telegram-bot-api';
+const TelegramBot = require('node-telegram-bot-api');
 
 const bot = new TelegramBot('6224834180:AAHEBS4E6TtB4_SytKjihUThzwAMQvndSvQ');
+
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const url = 'https://csgo500.com/rewards';
+
+axios.get(url)
+  .then(response => {
+    const $ = cheerio.load(response.data);
+    const isActive = $('.rain-box-container').hasClass('is-active');
+    console.log(isActive);
+  })
+  .catch(error => {
+    console.log(error);
+  });
 
 function checkRemoteWebsite() {
   puppeteer.launch()
@@ -22,4 +36,4 @@ function checkRemoteWebsite() {
     });
 }
 
-checkRemoteWebsite();
+// checkRemoteWebsite();
